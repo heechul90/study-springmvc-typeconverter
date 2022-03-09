46cf80eb6d908e022fba2c6a3f42ca2588e1b3c1
hclee <heechul4296@gmail.com>
2022-03-09 17:03:51 +0900

    스프링 타입 컨버터

---
+++ src/main/java/study/springmvc/typeconverter/controller/HelloController.java
@@ -0,0 +1,27 @@
+package study.springmvc.typeconverter.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/hello-v1")
+    public String helloV1(HttpServletRequest request) {
+        String data = request.getParameter("data");
+        Integer intValue = Integer.valueOf(data);
+        System.out.println("intValue = " + intValue);
+        return "ok";
+    }
+
+    @GetMapping(value = "/hello-v2")
+    public String helloV2(@RequestParam("data") Integer data) {
+        System.out.println("data = " + data);
+        return "ok";
+    }
+
+}
